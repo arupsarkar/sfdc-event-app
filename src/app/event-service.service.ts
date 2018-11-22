ced041b852570aad1a3ce52dfdfc33d945454fe6
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-11-22 14:15:48 -0500

    updated service

--- src/app/event-service.service.ts
+++ src/app/event-service.service.ts
@@ -9,7 +9,9 @@ import {LoginOAuth} from './model/LoginOAuth';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
-    'Access-Control-Allow-Origin': 'Yes'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Authorization, Content-Type, Accept'
   })
 };
 //  'Authorization': 'my-auth-token'
