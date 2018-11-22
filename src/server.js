117124fd83040e3ed224e2e75c4afc46d1fe9c8d
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-11-22 01:14:08 -0500

    Updated server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const app = express();
+const path = require('path');
 const corsOptions = {
   origin: 'http://localhost:4200',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
