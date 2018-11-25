1ed52929b08d1559936c2452c9114ae33b6e6884
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-11-24 21:30:00 -0500

    Adding remote URL for API calls

--- src/app/event-service.service.ts
+++ src/app/event-service.service.ts
@@ -11,7 +11,7 @@ const httpOptions = {
     'Content-Type':  'application/json',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Authorization, Content-Type, Accept'
+    'Access-Control-Allow-Headers': '*'
   })
 };
 @Injectable({
