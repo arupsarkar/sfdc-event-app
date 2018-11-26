60c0d72998e06df314349da639d999af68112552
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-11-26 16:01:01 -0500

    Adding res headers

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ const http = require('http');
 const path = require('path');
 
 const app = express();
-// Enable CORS
-app.use(cors());
-app.options('*', cors()); // include before other routes
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// // Enable CORS
+// app.use(cors());
+// app.options('*', cors()); // include before other routes
 // const whitelist = ['http://localhost:3000', 'https://login.salesforce.com'];
 // const corsOptions = {
 //   origin: function (origin, callback) {
@@ -21,20 +28,20 @@ app.options('*', cors()); // include before other routes
 //   }
 // };
 
-// Add headers
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next();
-});
+// // Add headers
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 // Initialize session
 app.use(session(
