dd4c5fb192f8c778f92b11d62e787d1225fa10df
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-11-26 16:16:03 -0500

    Added http header options with allow-headers

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,7 +9,7 @@ const httpOptions = {
     'Content-Type':  'application/json',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': '*'
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
   })
 };
 
