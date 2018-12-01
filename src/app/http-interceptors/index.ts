f04faa04e8fa8bdfb3c80e9f40e40df1749c4067
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-01 01:20:22 -0500

    Updated sequence of http interceptors

--- src/app/http-interceptors/index.ts
+++ src/app/http-interceptors/index.ts
@@ -6,5 +6,6 @@ import {AuthInterceptor} from './auth-interceptor';
 
 /** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, AuthInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
 ];
