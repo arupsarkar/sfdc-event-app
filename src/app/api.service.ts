4ad74dd42ab44b70bac186a0fb17ff2cd1da9c57
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-02 14:34:11 -0500

    Fixed error

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {catchError, map, tap} from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
@@ -52,7 +52,7 @@ export class ApiService {
       console.error(error); // log to console instead
 
       // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
+      console.log(`${operation} failed: ${error.message}`);
 
       // Let the app keep running by returning an empty result.
       return of(result as T);
