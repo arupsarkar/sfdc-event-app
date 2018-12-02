58a2b04cb48fa02ffe0898854d09e7fd26597532
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-02 12:38:47 -0500

    Fixed errors

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,7 +5,7 @@ import {LoginComponent} from '../login/login.component';
 import {CallbackComponent} from '../callback/callback.component';
 import {EventsComponent} from '../events/events.component';
 
-const routes: ROUTES = [
+const ROUTES = [
   // {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'api/oauth2/callback', component: CallbackComponent},
@@ -17,7 +17,7 @@ const routes: ROUTES = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(ROUTES)
   ],
   exports: [ RouterModule ]
 })
