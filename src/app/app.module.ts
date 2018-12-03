046dfeb840f7883eaa71684890a7054d0a548d19
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-03 09:17:26 -0500

    Added FormsModule to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {httpInterceptorProviders} from './http-interceptors';
 import { AppRoutingModule} from './app-routing/app-routing.module';
 import { MessagesComponent } from './messages/messages.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { EventDetailComponent } from './event-detail/event-detail.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [ApiService, CookieService, httpInterceptorProviders],
   bootstrap: [AppComponent]
