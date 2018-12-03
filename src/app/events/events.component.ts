44828507331c86447ebbad465a9e8eebe7b10f4a
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-02 22:01:31 -0500

    Added message in UX later

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -9,6 +9,8 @@ import { ApiService } from '../api.service';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
+  message = '';
+
   constructor(private route: ActivatedRoute, private  apiService: ApiService) { }
 
   ngOnInit() {
@@ -18,8 +20,11 @@ export class EventsComponent implements OnInit {
   publishEvents() {
     console.log('DEBUG: Publish Events', 'Start');
     console.log('DEBUG: Publish Events', 'End');
-    this.apiService.eventsPublish().subscribe( data => {console.log( 'DEBUG: Event Component : ', data ); },
-                                            response => {console.log('POST call in error', response); },
-                                            () => {console.log('The POST observable is now completed.'); });
+    this.apiService.eventsPublish().subscribe(
+                                        data => {
+                                              this.message = data;
+                                              console.log( 'DEBUG: Event Component : ', data ); },
+                                        response => {console.log('POST call in error', response); },
+                                      () => {console.log('The POST observable is now completed.'); });
   }
 }
