18dd455773e6716e4ccf633a077c4b21e98bad48
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-02 21:02:11 -0500

    Returning JSON from server

--- api.js
+++ api.js
@@ -51,7 +51,7 @@ router.post('/events/publish', (req, res, next) =>{
     console.log("DEBUG: Server - /events/publish : payload - ", ret);
     if (ret.success){
       console.log('DEBUG: Server - /events/publish : success - ', ret.success);
-      res.status(200).send( JSON.parse(ret) );
+      res.status(200).json( ret );
     }
   });
 });
