7531aa35b73372befa4ca2ec2c7336753d41b06e
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-03 00:32:44 -0500

    Returning 2 JSON PE from server

--- api.js
+++ api.js
@@ -1,21 +1,6 @@
 const express = require('express');
 const jsforce = require("jsforce");
 const router = express.Router();
-// const API = 'https://jsonplaceholder.typicode.com';
-const API = 'https://sfdc-api-app.herokuapp.com/api';
-
-
-//jsForce oauth2 connection
-const oauth2 = new jsforce.OAuth2({
-  // you can change loginUrl to connect to sandbox or prerelease env.
-  loginUrl : 'https://login.salesforce.com',
-  //clientId and Secret will be provided when you create a new connected app in your SF developer account
-  clientId : '3MVG9zlTNB8o8BA2wrVtTcGwhEwLCayBmMKJEF6uILig.M9wPX3IHZTlE8W7OsJKeJ0Mc0cHvIPF_p_bmMAXx',
-  clientSecret : '8963738011040646967',
-  redirectUri : 'http://localhost:3000/api/oauth2/callback'
-  // redirectUri : 'https://sfdc-event-app.herokuapp.com/api/oauth2/callback'
-});
-
 
 /* GET api listing. */
 router.get('/', (req, res) => {
@@ -24,7 +9,10 @@ router.get('/', (req, res) => {
 
 router.get('/getEvents', (req, res) => {
   console.log('DEBUG: SERVER: /events:');
-  res.status(200).send({label: 'Lead Event Bus', api_name: 'lead_event__e'});
+  res.status(200).json (
+      {label: 'Lead Event Bus', api_name: 'lead_event__e'},
+      {label: 'Case Event Bus', api_name: 'case_event__e'},
+    );
 });
 
 router.post('/events/publish', (req, res, next) =>{
