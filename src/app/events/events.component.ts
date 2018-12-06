3de27ff7b6cb60cb1a7cd96fa8fc5b969ea00e4e
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-06 08:39:13 -0500

    Removed old publish method

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -21,11 +21,11 @@ export class EventsComponent implements OnInit {
   publishEvents() {
     console.log('DEBUG: Publish Events', 'Start');
     console.log('DEBUG: Publish Events', 'End');
-    this.apiService.eventsPublish().subscribe(
-                                        data => {
-                                              this.message = JSON.stringify(data);
-                                              console.log( 'DEBUG: Event Component : ', data ); },
-                                        response => {console.log('POST call in error', response); },
-                                      () => {console.log('The POST observable is now completed.'); });
+    // this.apiService.eventsPublish().subscribe(
+    //                                     data => {
+    //                                           this.message = JSON.stringify(data);
+    //                                           console.log( 'DEBUG: Event Component : ', data ); },
+    //                                     response => {console.log('POST call in error', response); },
+    //                                   () => {console.log('The POST observable is now completed.'); });
   }
 }
