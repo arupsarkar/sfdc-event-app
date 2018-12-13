7b13d4e1b313f5b9bf374c76f7d81cd652f1ac2b
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-12 23:38:17 -0500

    Updated array to accept duplicates

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -5,12 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
   messages: string[] = [];
-  messagesSet: string[] = new Set[''];
 
   add(message: string) {
-    this.messagesSet.push(message);
-    this.messages = Array.from(new Set(this.messagesSet));
-    // this.messages.push(message);
+    this.messages.push(message);
   }
 
   clear() {
