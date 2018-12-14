160f3e4316f2e27f7e6d5988807156673ea8fdc1
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-14 14:38:06 -0500

    Added api error in service layer

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, of, throwError} from 'rxjs';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import {catchError, map, tap} from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
 
@@ -69,7 +69,8 @@ export class ApiService {
       tap( res => {
         this.log(`Logout result: ${JSON.stringify(res)}`);
       }),
-      catchError(this.handleError<any>(`Logout error `))
+      // catchError(this.handleError<any>(`Logout error `))
+      catchError(this.handleApiError)
     );
   }
 
@@ -88,6 +89,23 @@ export class ApiService {
       return of(result as T);
     };
   }
+
+  private handleApiError (error: HttpErrorResponse){
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+
 
   /** Log a EventService message with the MessageService */
   private log(message: string) {
