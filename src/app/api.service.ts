1396a107a07032835f8b91171dfd20dbc9abaad3
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-14 23:53:38 -0500

    this.log is taken out

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -108,19 +108,16 @@ export class ApiService {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
     } else {
-      this.log(`Error : ${JSON.stringify(error.error)}`);
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       console.error(
         `Backend returned code ${JSON.stringify(error.status)}, ` +
         `body was: ${JSON.stringify(error.error)}`);
-
     }
     // return an observable with a user-facing error message
     return throwError(
       'Something bad happened; please try again later.');
   }
-
 
   /** Log a EventService message with the MessageService */
   private log(message: string) {
