caaf2c09ba69fd8ec0050a30407bd1704933bf0e
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-15 00:34:31 -0500

    Concateneted error message

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -107,7 +107,6 @@ export class ApiService {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error.message);
     } else {
-      this.messageService.add(`ApiService: ${JSON.stringify(error.error)}`);
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       console.error(
