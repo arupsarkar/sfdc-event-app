53277210fd29649cc7376ba33f3b6e546ba64877
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-31 14:03:38 -0500

    Intercepting http responses in ng interceptor

--- src/app/http-interceptors/noop-interceptor.ts
+++ src/app/http-interceptors/noop-interceptor.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse
 } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import {tap} from 'rxjs/operators';
+import { MessageService } from '../message.service';
 
 /** Pass untouched request through to the next request handler. */
 @Injectable()
 export class NoopInterceptor implements HttpInterceptor {
 
+  constructor(private messageService: MessageService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
-    return next.handle(req);
+    return next.handle(req).pipe(
+      tap(
+        event => {
+          // logging http response to browser's console in case of success.
+          if (event instanceof HttpResponse) {
+            this.messageService.add(`${event.body}`);
+          }
+        },
+        error => {
+          // logging http response to browser's console in case of error.
+          if (error instanceof HttpResponse) {
+            this.messageService.add(`${error.status}`);
+            this.messageService.add(`${error.statusText}`);
+            this.messageService.add(`${error.type}`);
+          }
+        }
+      )
+    );
   }
 }
 
