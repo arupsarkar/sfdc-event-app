43a4cbdc4f9f52c237afb917f9f74f3ee5e383d0
Arup Sarkar <arup.sarkar@arupsarkar-ltm.internal.salesforce.com>
2018-12-31 16:59:40 -0500

    Refactored interceptors

--- src/app/http-interceptors/auth-interceptor.ts
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -1,13 +1,17 @@
 import { CookieService } from 'ngx-cookie-service';
-import {HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
+import { MessageService } from '../message.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private cookieService: CookieService) {}
+  constructor(private cookieService: CookieService,
+              private messageService: MessageService) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Get the auth token from the service.
     const access_token = this.cookieService.get('access_token');
     const instance_url = this.cookieService.get('instance_url');
@@ -20,6 +24,28 @@ export class AuthInterceptor implements HttpInterceptor {
     });
 
     // send cloned request with header to the next handler.
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(
+      tap(
+        event => {
+          if ( event instanceof HttpResponse) {
+            this.messageService.add('Auth Interceptor success response - Start');
+            this.messageService.add(`${event.body}`);
+            this.messageService.add('Auth Interceptor success response - End');
+          }
+        },
+        error => {
+          if (error instanceof HttpResponse || error instanceof HttpErrorResponse) {
+            this.messageService.add('Auth Interceptor error response - Start');
+            this.messageService.add(`${error.status}`);
+            this.messageService.add(`${error.statusText}`);
+            this.messageService.add(`${error.type}`);
+            this.messageService.add('Auth Interceptor error response - End');
+          }
+        },
+        () => {
+          this.messageService.add('Auth Interceptor response complete.');
+        }
+      )
+    );
   }
 }
