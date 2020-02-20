2911807b424fc9fef58dd0e7461aea052b165fdd
arupsarkar <sarkar.arup@gmail.com>
2020-02-20 13:12:28 -0500

    removed console.log

--- api.js
+++ api.js
@@ -177,7 +177,7 @@ router.post('/updateContact', (req, res, next) => {
   }, function(err, ret) {
     if (err || !ret.success) { res.status(200).json(err); }
     try{
-      console.log('producer send() : ', 'start' + JSON.stringify(req.producer));
+      console.log('producer send() : ', 'start');
       req.producer.send({
         topic: 'interactions',
         message: {
