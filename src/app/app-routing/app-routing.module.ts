c44b14804d104692527dd3e065ffb782da3a236c
arupsarkar <sarkar.arup@gmail.com>
2020-02-25 18:03:41 -0500

    Added landing route

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,15 +6,16 @@ import {CallbackComponent} from '../callback/callback.component';
 import {EventsComponent} from '../events/events.component';
 import {EventDetailComponent} from '../event-detail/event-detail.component';
 import {ContactsComponent} from '../contacts/contacts.component';
+import {LandingComponent} from '../landing/landing.component';
 
 const ROUTES = [
   // {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'api/oauth2/callback', component: CallbackComponent},
   {path: 'contacts', component: ContactsComponent},
-  // {path: 'events', component: EventsComponent},
-  // {path: 'event-detail/:fullName', component: EventDetailComponent},
-
+  {path: 'events', component: EventsComponent},
+  {path: 'event-detail/:fullName', component: EventDetailComponent},
+  {path: 'landing', component: LandingComponent},
 ];
 
 
